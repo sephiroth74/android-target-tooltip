785814fe833e1768e9ce20ef396277d26dae8535
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2016-05-02 14:14:59 -0300

    added option to enable and disable modality

--- library/src/main/java/it/sephiroth/android/library/tooltip/Tooltip.java
+++ library/src/main/java/it/sephiroth/android/library/tooltip/Tooltip.java
@@ -262,6 +262,7 @@ public final class Tooltip {
         private final Point mTmpPoint = new Point();
         private final Rect mHitRect = new Rect();
         private final float mTextViewElevation;
+        private boolean mModal;
         private Callback mCallback;
         private int[] mOldLocation;
         private Gravity mGravity;
@@ -430,6 +431,7 @@ public final class Tooltip {
             this.mCallback = builder.closeCallback;
             this.mFloatingAnimation = builder.floatingAnimation;
             this.mSizeTolerance = (int) (context.getResources().getDisplayMetrics().density * TOLERANCE_VALUE);
+            this.mModal = builder.modal;
 
             if (builder.typeface != null) {
                 mTypeface = builder.typeface;
@@ -1335,7 +1337,9 @@ public final class Tooltip {
                 if (ClosePolicy.touchOutside(mClosePolicy)) {
                     onClose(true, false, false);
                 }
-                return ClosePolicy.consumeOutside(mClosePolicy);
+                boolean consume = ClosePolicy.consumeOutside(mClosePolicy);
+                // if Modal is enabled, then capture the touch event
+                return consume && this.mModal;
 
             }
             return false;
@@ -1493,6 +1497,7 @@ public final class Tooltip {
         boolean overlay = true;
         AnimationBuilder floatingAnimation;
         Typeface typeface;
+        boolean modal = true;
 
         public Builder(int id) {
             this.id = id;
@@ -1676,6 +1681,17 @@ public final class Tooltip {
             }
             completed = true;
             overlay = overlay && gravity != CENTER;
+            return this;
+        }
+
+        /**
+         * Enables modality.
+         * @param md
+         * @return
+         */
+        public Builder modal(boolean md) {
+            throwIfCompleted();
+            this.modal = md;
             return this;
         }
     }
