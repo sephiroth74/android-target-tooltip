f3e2acaa45b28250440101e9062cca6add4b125b
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2019-01-16 17:23:36 -0500

    no toolbar

--- app/src/main/java/it/sephiroth/android/library/tooltip_demo/MainActivity.kt
+++ app/src/main/java/it/sephiroth/android/library/tooltip_demo/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         val metrics = resources.displayMetrics
 
